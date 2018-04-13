aab13e48fbd4aa195dcbae335eb36a1e9d1a3731
brandonao <brandonowensgd@gmail.com>
2018-04-13 10:35:02 -0700

    index.html

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+/*
+ Calculate when the next train will arrive; relative to current time.
+
+ current time + frequency = next arrival
+ ex: 5:10 + 25 = 5:35
+
+ next arrival - frequency = minutes away
+ ex: 5:35 - 25 = 10
+
+ convert HH:MM to MMMM;
+ 
+ */
